92457c6920e9c78cb1e0479da29add9bd6e5b478
JJ <jxu453@gatech.edu>
2019-09-02 15:37:37 -0400

    Initial Commit

---
+++ ProgrammingAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProgrammingAssignment
+//
+//  Created by Junjie Xu on 9/2/19.
+//  Copyright © 2019 Junjie Xu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
